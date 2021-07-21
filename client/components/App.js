699b699d7dbe421f6cd713f71535b620ce01877b
Maciej Pie <maciejpiech18@gmail.com>
2021-07-21 15:20:45 -0500

    building the skeleton

---
+++ client/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>this App component is working</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './client/index.js',
+  output: {
+    filename: './server/public/bundle.js',
+    path: __dirname,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+};
