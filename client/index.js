10b0a4095fcfe8d1df4b0fafea3cecea7d9f8126
Maciej Pie <maciejpiech18@gmail.com>
2021-07-21 20:52:56 -0500

    will continue working tomorrow

--- client/index.js
+++ client/index.js
@@ -4,10 +4,4 @@ import App from './components/App';
 
 const root = document.getElementById('app');
 
-ReactDOM.render(
-  <div>
-    <App />
-    <h1>this is a test</h1>
-  </div>,
-  root
-);
+ReactDOM.render(<App />, root);
