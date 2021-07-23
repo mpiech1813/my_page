8bbc17b7aacc90492c2460adb0531f4ce18e7561
Maciej Pie <maciejpiech18@gmail.com>
2021-07-23 14:24:14 -0500

    starting to work on react router

--- client/components/App.js
+++ client/components/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
+
 import Footer from './basic/Footer';
 import NavBar from './basic/Navbar';
+import About from './main/About';
+import Home from './main/Home';
+import Contact from './main/Contact';
+import Projects from './main/Projects';
 
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar></NavBar>
-        <h1>this App component is working</h1>
+
         <Footer />
       </div>
     );
---
+++ client/components/main/Projects.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+  render() {
+    <div>
+      <h1>This is Projects page</h1>
+    </div>;
+  }
+}
+
+export default Projects;
