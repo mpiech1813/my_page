edeb9d7e83a0f1ba1aff56ae2761829402cf0a30
Maciej Pie <maciejpiech18@gmail.com>
2021-07-23 13:54:05 -0500

    added skeleton to start working from

---
+++ client/components/basic/Navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+  render() {
+    return (
+      <div>
+        <button>Home</button>
+        <button>About Me</button>
+        <button>Projects</button>
+        <button>Contact Me</button>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
