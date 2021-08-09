3e74d91060b71d6c815fa47c194a00aa24bea106
mpiech1813 <maciejpiech18@gmail.com>
2021-08-09 11:20:39 -0500

    added faker paragraphs

--- client/components/main/Contact.js
+++ client/components/main/Contact.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import { Title } from '../../style';
+import faker from 'faker'
+import { element } from 'prop-types';
 
 class Contact extends Component {
   render() {
+    const dataArr = new Array(10).fill(faker.lorem.paragraphs())
+    let key =0
     return (
       <div>
         <Title>This is the Contact Section</Title>
+        <div style={{color:'yellow'}}>
+          {dataArr.map(element => {
+            key++
+            return <p key={key}>{element}</p>
+          })}
+        </div>
       </div>
     );
   }
