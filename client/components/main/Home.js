f5a3ecda300f9b480ea8692f4cfb56d54b94def9
mpiech1813 <maciejpiech18@gmail.com>
2021-08-09 11:52:54 -0500

    no changes

--- client/components/main/Home.js
+++ client/components/main/Home.js
@@ -1,24 +1,23 @@
 import React, { Component } from 'react';
 import { Title } from '../../style';
-import faker from 'faker'
-import { element } from 'prop-types';
+import faker from 'faker';
 
 class Home extends Component {
-  render() {
-    const dataArr = new Array(10).fill(faker.lorem.paragraphs())
-    let key = 0
-    return (
-      <div>
-        <Title>This is Home page</Title>
-        <div style ={{color:'blue'}}>
-        {dataArr.map(element => {
-          key ++
-          return <p key={key}>{element}</p>
-        })}
-        </div>
-      </div>
-    );
-  }
+    render() {
+        const dataArr = new Array(10).fill(faker.lorem.paragraphs());
+        let key = 0;
+        return (
+            <div>
+                <Title>This is Home page</Title>
+                <div style={{ color: 'blue' }}>
+                    {dataArr.map((element) => {
+                        key++;
+                        return <p key={key}>{element}</p>;
+                    })}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Home;
