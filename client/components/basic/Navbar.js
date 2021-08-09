4d4fe08f1c10ed413e21faa63b22e71908739274
mpiech1813 <maciejpiech18@gmail.com>
2021-08-09 12:53:21 -0500

    navbar is sticky

--- client/components/basic/Navbar.js
+++ client/components/basic/Navbar.js
@@ -1,29 +1,49 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Nav, MainLink } from '../../style';
+import { Nav, NavScrolled, MainLink } from '../../style';
 
-class NavBar extends Component {
-  render() {
+const NavBar = () => {
+    const [scrolled, setScrolled] = useState(false);
+
+    const handleScroll = () => {
+        const offset = window.scrollY;
+        if (offset > 200) {
+            setScrolled(true);
+        } else {
+            setScrolled(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+    });
+
+    let NavName = Nav;
+    if (scrolled) {
+        NavName = NavScrolled;
+    } else {
+        NavName = Nav;
+    }
+
     return (
-      <Nav>
-        <Link to="/" style={{ textDecoration: 'none' }}>
-          <MainLink>Home</MainLink>
-        </Link>
-        <Link to="/about" style={{ textDecoration: 'none' }}>
-          <MainLink>About Me</MainLink>
-        </Link>
-        <Link to="/projects" style={{ textDecoration: 'none' }}>
-          <MainLink>Projects</MainLink>
-        </Link>
-        <Link to="/achievements" style={{ textDecoration: 'none' }}>
-          <MainLink>Achievements</MainLink>
-        </Link>
-        <Link to="/contact" style={{ textDecoration: 'none' }}>
-          <MainLink>Contact Me</MainLink>
-        </Link>
-      </Nav>
+        <NavName>
+            <Link to='/' style={{ textDecoration: 'none' }}>
+                <MainLink>Home</MainLink>
+            </Link>
+            <Link to='/about' style={{ textDecoration: 'none' }}>
+                <MainLink>About Me</MainLink>
+            </Link>
+            <Link to='/projects' style={{ textDecoration: 'none' }}>
+                <MainLink>Projects</MainLink>
+            </Link>
+            <Link to='/achievements' style={{ textDecoration: 'none' }}>
+                <MainLink>Achievements</MainLink>
+            </Link>
+            <Link to='/contact' style={{ textDecoration: 'none' }}>
+                <MainLink>Contact Me</MainLink>
+            </Link>
+        </NavName>
     );
-  }
-}
+};
 
 export default NavBar;
--- client/style.js
+++ client/style.js
@@ -1,26 +1,38 @@
 import styled from 'styled-components';
 
 export const MainLink = styled.h1`
-  text-decoration: none;
-  font-size: 1.5;
-  color: green;
-  font-weight: bold;
+    text-decoration: none;
+    font-size: 1.5;
+    color: green;
+    font-weight: bold;
 `;
 export const Nav = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border: 1px solid red;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border: 1px solid red;
+    border-radius: 15px;
+    width: 100%;
+`;
 
-  border-radius: 15px;
+export const NavScrolled = styled.div`
+    position: fixed;
+    top: 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border: 1px solid red;
+    border-radius: 15px;
+    width: 100%;
+    background: #001a00;
 `;
 
 export const Title = styled.h1`
-  color: white;
+    color: white;
 `;
 
 export default {
-  Nav,
-  MainLink,
-  Title,
+    Nav,
+    MainLink,
+    Title,
 };
