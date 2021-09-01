e20a9c460e4f262f60aa60d3a602b06f12d061b5
mpiech1813 <maciejpiech18@gmail.com>
2021-09-01 18:56:48 -0500

    will need to set up redux

--- client/components/basic/Navbar.js
+++ client/components/basic/Navbar.js
@@ -1,31 +1,50 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { Nav, MainLink, Br } from '../../style';
 
+const useOnScreen = (ref) => {
+    const [isIntersectiong, setIntersecting] = useState(false);
+
+    const observer = new IntersectionObserver(([entry]) =>
+        setIntersecting(entry.isIntersecting)
+    );
+
+    useEffect(() => {
+        observer.observe(ref.current);
+        return () => {
+            observer.disconnect();
+        };
+    }, []);
+    return isIntersectiong;
+};
+
 const NavBar = () => {
+    const ref = useRef();
+    const isVisible = useOnScreen(ref);
+
     return (
         <Nav>
             <img src='/public/assets/logo.png' width='130px' alt='Logo' />
             <Link to='home' style={{ textDecoration: 'none' }}>
                 <MainLink>Home</MainLink>
-                <Br />
+                {isVisible ? <Br /> : <h1>notvisible</h1>}
             </Link>
 
             <Link to='about' style={{ textDecoration: 'none' }}>
                 <MainLink>About Me</MainLink>
-                <Br />
+                <Br ref={ref} />
             </Link>
             <Link to='projects' style={{ textDecoration: 'none' }}>
                 <MainLink>Projects</MainLink>
-                <Br />
+                <Br ref={ref} />
             </Link>
             <Link to='achievements' style={{ textDecoration: 'none' }}>
                 <MainLink>Achievements</MainLink>
-                <Br />
+                <Br ref={ref} />
             </Link>
             <Link to='contact' style={{ textDecoration: 'none' }}>
                 <MainLink>Contact Me</MainLink>
-                <Br />
+                <Br ref={ref} />
             </Link>
         </Nav>
     );
