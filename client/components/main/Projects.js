9ed933992550f3c0d58a5d8c2725cc853331c5c5
mpiech1813 <maciejpiech18@gmail.com>
2021-10-23 16:12:40 -0500

    added text

--- client/components/main/Projects.js
+++ client/components/main/Projects.js
@@ -1,6 +1,6 @@
 import React, { Component, useRef } from 'react';
 import { connect } from 'react-redux';
-import { Title, Break } from '../../style';
+import { Title, Break, PBlock } from '../../style';
 
 class Projects extends Component {
     render() {
@@ -8,6 +8,32 @@ class Projects extends Component {
             <div id='projects'>
                 <Break />
                 <Title>This is Projects page</Title>
+                <h2>BOILERPLATE BUILDER (CAPSTONE PROJECT) | JULY 2021</h2>
+                <h3>Description: </h3>
+                <PBlock>Template creator for the PERN Stack</PBlock>
+                <h3>Contributions: </h3>
+                <PBlock>Backend, log in, Github OAuth, boilerplates</PBlock>
+                <h3>Technologies: </h3>
+                <PBlock>
+                    Node.js | PostgreSQL | Sequelize | React.js | React-Redux |
+                    GitHub API | EJS | Web Sockets | JWT
+                </PBlock>
+                <br />
+                <h2>NOTE-YT (PERSONAL PROJECT) | MAY 2021</h2>
+                <h3>Description: </h3>
+                <PBlock>
+                    Firefox extension for taking notes on YouTube videos
+                </PBlock>
+                <h3>Technology: </h3>
+                <PBlock>Firefox browser engine</PBlock>
+
+                <h2>HOW-THE-TURNTABLES (GROUP PROJECT) | MAY 2021</h2>
+                <h3>Description: </h3>
+                <PBlock>E-commerce store for bootleg records</PBlock>
+                <h3>Contributions: </h3>
+                <PBlock>Log-in, backend, artist’s view</PBlock>
+                <h3>Technologies: </h3>
+                <PBlock>Node.js, Sequelize, React.js, Stripe API</PBlock>
             </div>
         );
     }
