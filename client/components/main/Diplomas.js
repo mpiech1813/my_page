0410a1f35821c93d7916bce6130f82eeded448c6
mpiech1813 <maciejpiech18@gmail.com>
2021-10-24 10:21:03 -0500

    going to add education section

--- client/components/main/Diplomas.js
+++ client/components/main/Diplomas.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
 import StyCom, { Title, Break } from '../../style';
-import faker from 'faker';
 
 class Diplomas extends Component {
     render() {
-        const dataArr = new Array(20).fill(faker.lorem.paragraphs());
         return (
             <div id='achievements'>
                 <Break />
                 <Title>This is the Achievement page</Title>
-                <div style={{ color: 'red' }}>
-                    {dataArr.map((element, i) => {
-                        return <p key={i}>{element}</p>;
-                    })}
-                </div>
             </div>
         );
     }
