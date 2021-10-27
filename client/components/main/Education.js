abe1f514ea46852714f68075c13910f84ee79cc7
mpiech1813 <maciejpiech18@gmail.com>
2021-10-27 16:53:34 -0500

    fixed spaciong

--- client/components/main/Education.js
+++ client/components/main/Education.js
@@ -4,28 +4,30 @@ import StyCom, { Title } from '../../style';
 class Education extends Component {
     render() {
         return (
-            <StyCom.HomePage id='achievements'>
+            <div>
                 <StyCom.Break />
-                <h1>Education</h1>
-                <Title>
-                    FULLSTACK DEVELOPER | <strong>FULLSTACK ACADEMY</strong>
-                </Title>
-                <h3>January 2021 — July 2021</h3>
-                <ul>
-                    <li>
-                        553 hours of rigorous software engineering bootcamp
-                        based on group projects and peer programming
-                    </li>
-                </ul>
-                <br />
-                <Title>
-                    BACHELOR OF SCIENCE | <strong>LOYOLA UNIVERSITY</strong>
-                </Title>
-                <h3>2009 - 2013</h3>
-                <ul>
-                    <li>Major: Forensic Science | Minor: Chemistry</li>
-                </ul>
-            </StyCom.HomePage>
+                <StyCom.HomePage id='achievements'>
+                    <h1>Education</h1>
+                    <Title>
+                        FULLSTACK DEVELOPER | <strong>FULLSTACK ACADEMY</strong>
+                    </Title>
+                    <h3>January 2021 — July 2021</h3>
+                    <ul>
+                        <li>
+                            553 hours of rigorous software engineering bootcamp
+                            based on group projects and peer programming
+                        </li>
+                    </ul>
+                    <br />
+                    <Title>
+                        BACHELOR OF SCIENCE | <strong>LOYOLA UNIVERSITY</strong>
+                    </Title>
+                    <h3>2009 - 2013</h3>
+                    <ul>
+                        <li>Major: Forensic Science | Minor: Chemistry</li>
+                    </ul>
+                </StyCom.HomePage>
+            </div>
         );
     }
 }
