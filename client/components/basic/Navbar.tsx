f399de48894ef75e0f9466bbb8d6faee2ba807b4
mpiech1813 <maciejpiech18@gmail.com>
2022-08-17 10:35:40 -0500

    fixed the order

--- client/components/basic/Navbar.tsx
+++ client/components/basic/Navbar.tsx
@@ -12,17 +12,16 @@ const NavBar = () => {
                 <MainLink>Contact Me</MainLink>
                 <Br />
                 </Link>
-
                 <Link to='about' style={{ textDecoration: 'none' }}>
                 <MainLink>About Me</MainLink>
                 <Br />
                 </Link>
-                <Link to='projects' style={{ textDecoration: 'none' }}>
-                <MainLink>Projects</MainLink>
-                <Br />
-                </Link>
                 <Link to='contact' style={{ textDecoration: 'none' }}>
                 <MainLink>Work Experience</MainLink>
+                <Br />
+                </Link>
+                <Link to='projects' style={{ textDecoration: 'none' }}>
+                <MainLink>Projects</MainLink>
                 <Br />
                 </Link>
                 <Link to='achievements' style={{ textDecoration: 'none' }}>
