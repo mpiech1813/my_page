389d2e28e28d139af15fa15bb71364572a356adc
mpiech1813 <maciejpiech18@gmail.com>
2022-08-17 11:02:59 -0500

    removed unnecessary extensions

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,14 +1,14 @@
 import React, { Component, Fragment } from "react";
 import StickyBox from "react-sticky-box";
 
-import { MainContainer } from "../style.js";
+import { MainContainer } from "../style";
 import Footer from "./basic/Footer";
 import NavBar from "./basic/Navbar";
-import About from "./main/About.js";
-import Contact from "./main/Contact.js";
-import Work from "./main/Work.js";
-import Projects from "./main/Projects.js";
-import Education from "./main/Education.js";
+import About from "./main/About";
+import Contact from "./main/Contact";
+import Work from "./main/Work";
+import Projects from "./main/Projects";
+import Education from "./main/Education";
 
 class App extends Component {
   render() {
